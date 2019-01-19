3e3058f8d175c165920c9c9b06215ef8fcd37b8e
muskmelon9527 <43705464+muskmelon9527@users.noreply.github.com>
2019-01-19 11:04:21 +0800

    delete

--- app/helpers/users_helper.rb
+++
@@ -1,5 +0,0 @@
-module UsersHelper
-  def available_user
-    return  @users- [User.find_by_name("无作者")]
-  end
-end
--- app/models/message.rb
+++
@@ -1,7 +0,0 @@
-class Message < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :chat
-
-  sync :all
-  sync_scope :by_chat, ->(chat) { where(chat_id: chat.id) }
-end
